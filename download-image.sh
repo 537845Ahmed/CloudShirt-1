977d1d00e6f65049d8900e1e31b84cfdff3d4773
LucaKasanardjo <118907054+LucaKasanardjo@users.noreply.github.com>
2025-10-21 14:43:49 +0200

    Update download-image.sh

--- download-image.sh
+++ download-image.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 730335381450.dkr.ecr.us-east-1.amazonaws.com
-docker pull 730335381450.dkr.ecr.us-east-1.amazonaws.com/eshopwebmvc:latest
-docker pull 730335381450.dkr.ecr.us-east-1.amazonaws.com/eshoppublicapi:latest
+
+ECR_URL="730335381450.dkr.ecr.us-east-1.amazonaws.com"
+IMAGES=("eshopwebmvc:latest" "eshoppublicapi:latest")
+REGION="us-east-1"
+
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ECR_URL
+
+for IMAGE in "${IMAGES[@]}"; do
+  SUCCESS=0
+  while [ $SUCCESS -eq 0 ]; do
+    echo "Pulling $IMAGE..."
+    docker pull "$ECR_URL/$IMAGE"
+    if docker images | grep -q "${IMAGE%:*}"; then
+      echo "$IMAGE gepulled!"
+      SUCCESS=1
+    else
+      echo "$IMAGE nog niet beschikbaar..."
+      sleep 10
+    fi
+  done
+done
+
+echo "IMAGES SUCCESVOL GEPULLED YIPPIE"
